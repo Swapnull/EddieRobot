867cedc267dd1e4658f0af7a30100552fa91bb9f
Kevlangelo <b2018696@my.shu.ac.uk>
2016-03-06 14:11:40 +0000

    Remove /build and /devel from repo
    The buld and devel folders should no have been included in the
    last commit, this one removes them

--- build/eddie_serial_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kevlangelo/eddie_serial_driver/src/eddie_serial_driver
-# Build directory: /home/kevlangelo/eddie_serial_driver/build/eddie_serial_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/kevlangelo/eddie_serial_driver/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
